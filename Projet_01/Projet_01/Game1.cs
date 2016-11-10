a2354d370dea993a661197032dab96223e52a027
lemelinjs <lemelin.jean-sebastien@carrefour.cegepvicto.ca>
2016-11-10 15:40:47 -0500

    Ajout de l'objet
    J'ai créé un objet qui permet de lire la postion en x, y, le centre x,
    le centre y et la valeur estVivant.
    
    Je cherche toujours une manière d'avoir accès à la librairie Content...
    sans succès (pour pouvoir appeler les images)

--- Projet_01/Projet_01/Game1.cs
+++ Projet_01/Projet_01/Game1.cs
@@ -3,11 +3,24 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 /* Initialiser les variables dans public class Game1: Game
- * Loader le contenenu (instancier) - hero =new GameObjet
- * Rectangle fenetre;
+ * GameObject hero;
+ * dans LoadContent()
+ * hero = new GameObjet;
+ * On initialise la grosseur de la fenete dans fonction Initialise()
+ * On trace un rectangle transparent au dessus de la fenetre
+ * Rectangle fenetre à mettre sur le jeu
+ * L'action se passe dans Update()
+ * Dans Draw() on dessine ce qu'on veut voir apparaitre;
+ * tHÉORIE DU 10
+ * Récap
+ * Étape 1
+ * Créer l'objet globalement
+ * Loadcontent : on met des trucs dans notre gameobject
+ * On ne met presque rien dans update, on appelle des fonctions
  */
 namespace Projet_01
 {
+    
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
@@ -17,8 +30,15 @@ namespace Projet_01
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         GameObject hero;
+        GameObject ennemi;
         GameObject fond;
+        GameObject screen;
         Rectangle fenetre;
+        public static int maxX;
+        public static int maxY;
+
+
+
 
         public Game1()
         {
@@ -40,11 +60,14 @@ namespace Projet_01
             this.graphics.PreferredBackBufferWidth = graphics.GraphicsDevice.DisplayMode.Width;
             this.graphics.PreferredBackBufferHeight = graphics.GraphicsDevice.DisplayMode.Height;
 
-            // Met toi en plein écran
-            //this.graphics.ToggleFullScreen();
-            this.graphics.ApplyChanges();
+            maxY = graphics.GraphicsDevice.DisplayMode.Height;
+            maxX = graphics.GraphicsDevice.DisplayMode.Width;
 
-            fenetre = new Rectangle(0, 0, graphics.GraphicsDevice.DisplayMode.Width, graphics.GraphicsDevice.DisplayMode.Height);
+            // Met toi en plein écran
+            this.graphics.ToggleFullScreen();
+            //this.graphics.ApplyChanges();
+
+            fenetre = new Rectangle(0, 0, maxX, maxY);
             base.Initialize();
         }
 
@@ -56,14 +79,18 @@ namespace Projet_01
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            hero = new GameObject();
+            hero = new GameObject("avionHero.png",300, 300, 150, 96, true);
+            ennemi = new GameObject("wildcat-top.png",500,500,75,100, true);
             fond = new GameObject();
 
-            hero.estVivant = true;
-            hero.position.X = 0; // 00 en haut à gauche
-            hero.position.Y = 0; // 00 en haut à gauche
+            //screen = new GameObject();
             fond.sprite = Content.Load<Texture2D>("fond.jpg");
+
             hero.sprite = Content.Load<Texture2D>("avionHero.png");
+            ennemi.sprite = Content.Load<Texture2D>("wildcat-top.png");
+
+
+            hero.rotationAngle = 0;
 
             // TODO: use this.Content to load your game content here
         }
@@ -84,29 +111,78 @@ namespace Projet_01
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || 
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
             {
-                hero.position.Y -= 5;
+                hero.position.Y -= 20;
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
             {
-                hero.position.Y += 5;
+                hero.position.Y += 20;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.A))
             {
                 hero.position.X -= 5;
+            }
+            //Recule rapide
+            if (Keyboard.GetState().IsKeyDown(Keys.CapsLock))
+            {
+                hero.position.X -= 30;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.D))
             {
                 hero.position.X += 5;
             }
 
+            //Détection des limites
+
+            if ((hero.position.Y) > (maxY-96))
+            {
+                hero.position.Y = -96;
+            }
+            else if ((hero.position.Y) < -96)
+            {
+                hero.position.Y = maxY-96;
+            }
+            if ((hero.position.X + 300) >= (int)(maxX/2))
+            {
+                hero.position.X -= 5;
+            }
+            else if (hero.position.X < 150)
+            {
+                hero.position.X = 150;
+            }
+
+            // rotation essais
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                hero.rotationAngle -= 0.03f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                hero.rotationAngle += 0.03f;
+            }
+            if (hero.rotationAngle < -1.0f)
+            {
+                hero.rotationAngle = -1.0f;
+            }
+            if (hero.rotationAngle > 1.2f)
+            {
+                hero.rotationAngle = 1.2f;
+            }
+
 
             // TODO: Add your update logic here
-
+            UpdateHero(); // un update pour le vaisseau
             base.Update(gameTime);
+        }
+
+        ///
+        public void UpdateHero()
+        {
+
         }
 
         /// <summary>
@@ -115,14 +191,14 @@ namespace Projet_01
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
+            // fond bleu de base
             GraphicsDevice.Clear(Color.CornflowerBlue);
             // L'objet qui permet d'afficher toutes les images 
             spriteBatch.Begin();
-            spriteBatch.Draw(fond.sprite, new Vector2(0, 0), Color.White);
-            spriteBatch.Draw(hero.sprite, hero.position, Color.White);
-
-
-
+            spriteBatch.Draw(fond.sprite, new Rectangle(0, 0, maxX,maxY), Color.White);
+            //spriteBatch.Draw(hero.sprite, hero.position, Color.White);
+            spriteBatch.Draw(hero.sprite,hero.position, null, Color.White, hero.rotationAngle, hero.origine, 1.0f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(ennemi.sprite, ennemi.position, Color.White);
 
 
 
--- Projet_01/Projet_01/GameObject.cs
+++ Projet_01/Projet_01/GameObject.cs
@@ -13,6 +13,51 @@ namespace Projet_01
         public bool estVivant;
         public Texture2D sprite;
         public Vector2 position;
+        public Vector2 origine;
         public Vector2 vitesse;
+        public float rotationAngle;
+
+        public Rectangle rectColision = new Rectangle();
+
+
+        public GameObject(String lienImage, int positionX, int positionY, int origineX, 
+            int origineY, bool estVivant)
+        {
+            //this.sprite = new Game1().Content.Load<Texture2D>(lienImage); //encore en erreur
+            this.position.X = positionX;
+            this.position.Y = positionY;
+            this.origine.X = origineX;
+            this.origine.Y = origineY;
+            this.estVivant = true;
+        }
+        public GameObject()
+        {
+            //this.sprite = new Game1().Content.Load<Texture2D>(lienImage); // Encore en erreur
+
+        }
+        public GameObject(int positionX, int positionY, bool estVivant)
+        {
+           // this.sprite = new Game1().Content.Load<Texture2D>(lienImage);//Encore en erreur
+            this.position.X = positionX;
+            this.position.Y = positionY;
+            this.estVivant = true;
+        }
+
+        
+
+
+        public Rectangle GetRect() //1 Pour sauver plusieurs lignes de code
+        {
+            rectColision.X = (int) this.position.X;
+            rectColision.Y = (int) this.position.Y;
+            rectColision.Height = this.sprite.Height;
+            rectColision.Width = this.sprite.Width;
+
+            return rectColision;
+        }
+
+
+        public int Width { get; internal set; }
+        public int Height { get; internal set; }
     }
 }
