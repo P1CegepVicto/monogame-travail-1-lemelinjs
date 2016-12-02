bf0692eda6a3ef57efb2b4e7c4eec63fe46993fd
lemelinjs <lemelin.jean-sebastien@carrefour.cegepvicto.ca>
2016-12-02 07:28:56 -0500

    Musique de fond et effets sonnores
    Coup de tire quand l'ennemi est touché. Son d'écrasement au 4e tir. Je
    n'arrive pas à mettre le trigger de 5 secondes à la fin de la game dans
    UpdateMissile(gameTime) ni a updater mon message pour qu'il update mon
    message

--- Projet_01/Projet_02/Game1.cs
+++ Projet_01/Projet_02/Game1.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Threading;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Projet_02
 {
@@ -14,18 +17,28 @@ namespace Projet_02
         SpriteBatch spriteBatch;
         public Rectangle fenetre;
         public Random nombre = new Random();
+        public int nbVies = 0;
+        private bool gameOver;
+        int tempsFrappe = 0;
         GameObject fond;
         GameObject[] nuage;
         GameObject hero;
         GameObject heroEnFeu;
+        GameObject [] heroEnFume;
         GameObject[] ennemi;
         GameObject[] missile;
         SpriteFont font;
+
+        // Les sons
+        SoundEffect son;
+        SoundEffectInstance bombe;
+        SoundEffectInstance bombe1;
         // Parametres de la game
 
         int nombreEnnemisVivant = 0, nombreEnnemiMax = 4;
         int ennemiQuiTire;
-        private int missileEnCourse = 0, nbMissilesSup = 0;
+        int missileEnCourse = 0, nbMissilesSup = 0;
+        String message;
 
 
         public Game1()
@@ -54,7 +67,8 @@ namespace Projet_02
                 graphics.GraphicsDevice.DisplayMode.Height);
             this.graphics.ToggleFullScreen();
             //this.graphics.ApplyChanges();
-
+            message = "La fenetre fait " + fenetre.Width + " de large \n" +
+                "La fenetre fait " + fenetre.Height + " de haut";
             base.Initialize();
         }
 
@@ -64,34 +78,71 @@ namespace Projet_02
         /// </summary>
         protected override void LoadContent()
         {
+            
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-           
+
+            // Le sont d'un missile non mortel
+            son = Content.Load<SoundEffect>("Sons\\Bombe1");
+            bombe1 = son.CreateInstance();
+
+            // Le son du dernier missile mortel
+            son = Content.Load<SoundEffect>("Sons\\Bombe");
+            bombe = son.CreateInstance();
+
+            
+
+            //La musique de fond
+            Song song = Content.Load<Song>("Sons\\BrotherInArms");
+            MediaPlayer.Play(song);
+            
             // Le héro
             //--------
-            hero = new GameObject(nombre.Next(0,fenetre.Width),
-                nombre.Next(0, fenetre.Height), true);
+            hero = new GameObject(fenetre.Width/2,
+               fenetre.Height/2, true);
             hero.sprite = Content.Load<Texture2D>("Hero/avionHero.png");
             // Le centre de l'image
             hero.origine.X = hero.sprite.Width/2;
             hero.origine.Y = hero.sprite.Height/2;
-            hero.angleRotation = 0f;
+            hero.angleRotation = -1.5708f;
 
             //Le héro en feu
             heroEnFeu = new GameObject(hero.position.X,
                 hero.position.Y, false);
             heroEnFeu.sprite = Content.Load<Texture2D>("Hero/avionHeroFeu.png");
 
+            // Le héro en fumé
+            heroEnFume = new GameObject[3];
+
+            heroEnFume[0] = new GameObject(hero.position.X-200,
+                hero.position.Y, false);
+            heroEnFume[0].sprite = Content.Load<Texture2D>("Hero/fume.png");
+
+            heroEnFume[1] = new GameObject(hero.position.X-100,
+               hero.position.Y, false);
+            heroEnFume[1].sprite = Content.Load<Texture2D>("Hero/fume.png");
+
+            heroEnFume[2] = new GameObject(hero.position.X,
+               hero.position.Y, false);
+            heroEnFume[2].sprite = Content.Load<Texture2D>("Hero/fume.png");
+
+
             // Le fond
             fond = new GameObject();
-            fond.sprite = Content.Load<Texture2D>("Fond/water.jpg");
+            fond.sprite = Content.Load<Texture2D>("Fond/fond.jpg");
+            fond.position.X = hero.position.X;
+            fond.position.Y = hero.position.Y;
+
+            fond.origine.X = fond.sprite.Width/2;
+            fond.origine.Y = fond.sprite.Height/ 2;
+
 
             // Les nuages
             nuage = new GameObject[10];
             for (int i = 0; i < 10; i++)
             {
-                nuage[i] = new GameObject(nombre.Next(0, fenetre.Width),
-                nombre.Next(0, fenetre.Height), true);
+                nuage[i] = new GameObject(nombre.Next(0 - (fenetre.Width *2), fenetre.Width  *2),
+                nombre.Next(0 - (fenetre.Height *2), fenetre.Height * 2), true);
                 nuage[i].sprite = Content.Load<Texture2D>("Fond/nuage.png");
             }
 
@@ -150,38 +201,123 @@ namespace Projet_02
                 Exit();
 
           if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+
+               
+                if (fond.angleRotation >= -1.5708 && fond.angleRotation < 0.7854)
                 {
-                    hero.position.Y -= 20;
+                    fond.origine.Y = fond.origine.Y - 3;
+                    
                 }
-                if (Keyboard.GetState().IsKeyDown(Keys.S))
+                else if (fond.angleRotation >= -0.7854 && fond.angleRotation < 0.7854)
                 {
-                    hero.position.Y += 20;
+                    fond.origine.Y = fond.origine.Y - 15;
                 }
+                else if (fond.angleRotation >= -1.5708 && fond.angleRotation < -0.7854)
+                {
+                    fond.origine.Y = fond.origine.Y - 3;
+                }
+                
+                // reculer le décor au lieu d'avancer condition décor vers vers la gauche (fleche de droite)
+                if (fond.angleRotation >= 1.5708 && fond.angleRotation < 2.3562) // Fonctionne
+                {
+                    fond.origine.Y = fond.origine.Y + 3;
+                }
+                else if (fond.angleRotation >= 2.3562 && fond.angleRotation < 3.927 ) 
+                {
+                    fond.origine.Y = fond.origine.Y + 15;
+                }
+                else if (fond.angleRotation >= 3.927 && fond.angleRotation < 4.7124 )
+                {
+                    fond.origine.Y = fond.origine.Y + 3;
+                }
+
+                // Reculer le décor consition le décor vers la droite (flèche vers la gauche)
+                if (fond.angleRotation >= -2.3562 && fond.angleRotation < -1.5708)    // à vérifier
+                {
+                    fond.origine.Y = fond.origine.Y + 3;
+                }
+                else if (fond.angleRotation >= 2.3562 && fond.angleRotation <  3.927)
+                {
+                    fond.origine.Y = fond.origine.Y + 15;
+                }
+                else if ( fond.angleRotation >= -4.7124 && fond.angleRotation < -3.927) // à vérifier
+                {
+                    fond.origine.Y = fond.origine.Y + 3;
+                }
+
+                UpdateNuages(1);
+                }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+
+                if (fond.angleRotation > -4.7124 && fond.angleRotation <= -3.927)
+                {
+                    fond.origine.Y = fond.origine.Y + 3;
+                }
+                else if (fond.angleRotation >= -2.2562 && fond.angleRotation <= 0.7854)
+                {
+                    fond.origine.Y = fond.origine.Y + 15;
+                }
+                else if (fond.angleRotation > 0.7854 && fond.angleRotation < 1.5708)
+                {
+                    fond.origine.Y = fond.origine.Y + 3;
+                }
+
+
+
+                //                if (fond.vitesse.Y < 0.1f)
+                //                {
+                //                    fond.vitesse.Y = 0.4f;
+                //                }
+
+                UpdateNuages(2);
+             }
                 if (Keyboard.GetState().IsKeyDown(Keys.A))
                 {
-                    hero.position.X -= 20;
+                    fond.origine.X -= 15;
+
+                    fond.vitesse.X -= 0.1f;
+                    
+                    UpdateNuages(3);
                 }
 
                 if (Keyboard.GetState().IsKeyDown(Keys.D))
                 {
-                    hero.position.X += 20;
-                }
 
-                // Rotation de l'avion
+                    fond.origine.X -= 15;
+                    fond.vitesse.X += 0.1f;
+                    
+                    UpdateNuages(4);
+                 }
+
+                // Rotation de l'écran
                 if (Keyboard.GetState().IsKeyDown(Keys.Left))
                 {
-                    hero.angleRotation += -0.1f;
+                    fond.angleRotation += 0.01f;
+                    if (fond.angleRotation > 4.7124f)
+                    {
+                        fond.angleRotation = -1.5708f;
+                    }
                 }
                 if (Keyboard.GetState().IsKeyDown(Keys.Right))
                 {
-                    hero.angleRotation += 0.1f;
-                }
+                    fond.angleRotation += -0.01f;
+                    if (fond.angleRotation < -4.7124f)
+                    {
+                        fond.angleRotation = 1.5708f;
+                    }
+            }
+            //fond.origine += fond.vitesse;
 
+            
 
             // TODO: Add your update logic here
             UpdateHero();
             UpdateEnnemi();
             UpdateMissiles();
+            UpdateMissiles(gameTime);
             UpdateEnnemi(gameTime);
             UpdateHero(gameTime);
             base.Update(gameTime);
@@ -190,6 +326,42 @@ namespace Projet_02
         public void UpdateHero()
         {
             hero.InScreen(fenetre);
+        }
+
+        public void UpdateNuages(int direction)
+        {
+            // UP
+            if (direction ==1)
+            {
+                for (int i = 0; i < nuage.Length; i++)
+                {
+                    nuage[i].position.Y += 12;
+                }
+            }
+            // DOWN
+            else if (direction == 2)
+            {
+                for (int i = 0; i < nuage.Length; i++)
+                {
+                    nuage[i].position.Y -= 12;
+                }
+            }
+            else if (direction == 3)
+            {
+                for (int i = 0; i < nuage.Length; i++)
+                {
+                    nuage[i].position.X += 12;
+                }
+            }
+            else if (direction == 4)
+            {
+                for (int i = 0; i < nuage.Length; i++)
+                {
+                    nuage[i].position.X -= 12;
+                }
+            }
+
+
         }
 
         public void UpdateEnnemi()
@@ -353,21 +525,56 @@ namespace Projet_02
             else
             {
 
-                //Donner l'accélération au missile lancé
-                missile[ennemiQuiTire * 2].position.X 
-                += missile[ennemiQuiTire * 2].vitesse.X;
+                //Donner l'accélération au missile 
+                if (missile[ennemiQuiTire * 2].isLaunched)
+                {
+                    missile[ennemiQuiTire * 2].position.X
+                        += missile[ennemiQuiTire * 2].vitesse.X;
+                }
 
                 // Vérifier la collision
                 if (missile[ennemiQuiTire * 2].GetRect().Intersects(hero.GetRect()))
                 {
-                    heroEnFeu.estVivant = true;
+
+                    //Afficher la fumée
+                    for (int i = 0; i < heroEnFume.Length; i++)
+                    {
+                        if (!heroEnFume[i].estVivant )
+                        {
+                            if (i==0)
+                            {
+                                heroEnFume[i].estVivant = true;
+                                i = heroEnFume.Length;
+                            }
+                            else
+                            {
+                                heroEnFume[i + 1].estVivant = true;
+                                i = heroEnFume.Length;
+                            }
+                        }
+                    }
+
+                    // Préparer la fin de la game ou le recommencement
+                    if (nbVies == 4)
+                    {
+                        bombe.Play();
+                        gameOver = true;
+                        
+                    }
+                    else
+                    {
+                        bombe1.Play();
+                        nbVies++;    
+                    }
+
+                    missile[ennemiQuiTire * 2].isLaunched = false;
+                    missileEnCourse--;
+
+
                 }
               
-                if (missile[ennemiQuiTire * 2].isLaunched)
-                {
-                    missile[ennemiQuiTire * 2].position.X 
-                        += missile[ennemiQuiTire * 2].vitesse.X;
-                }
+                
+                // Detecter un missile hors de la fenêtre
                 if (missile[ennemiQuiTire * 2].isLaunched &
                     missile[ennemiQuiTire * 2].position.X < 0)
                 {
@@ -395,6 +602,20 @@ namespace Projet_02
 
         }
 
+        public void UpdateMissiles(GameTime gameTime)
+        {
+
+            if (gameOver = true && tempsFrappe != 0)
+            {
+                tempsFrappe = gameTime.TotalGameTime.Milliseconds;
+                message = "frappé a " + tempsFrappe;
+            }
+            else if (gameTime.TotalGameTime.Milliseconds - tempsFrappe > 5000  && gameOver == true) //  si 5 seconde se sont écoulés depuis gameOver
+            {
+                Exit();
+            }
+        }
+
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
@@ -404,10 +625,11 @@ namespace Projet_02
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            
             spriteBatch.Begin();
             // L'eau
-            spriteBatch.Draw(fond.sprite, new Rectangle(0, 0, fenetre.Width, fenetre.Height), 
-                Color.White);
+            spriteBatch.Draw(fond.sprite, fond.position, null,  Color.White,
+                fond.angleRotation,fond.origine,1.0f, SpriteEffects.None,0f);
 
             // LEs 10 nuages
             for (int i = 0; i < 10; i++)
@@ -421,11 +643,15 @@ namespace Projet_02
             spriteBatch.Draw(hero.sprite, hero.position, null, Color.White,
                 hero.angleRotation, hero.origine, 1.0f, SpriteEffects.None, 0f);
             // Le feu si frappé
-            if (heroEnFeu.estVivant)
+            for (int i = 0; i < heroEnFume.Length; i++)
             {
-                spriteBatch.Draw(heroEnFeu.sprite, hero.position, null, Color.White,
-                hero.angleRotation, hero.origine, 1.0f, SpriteEffects.None, 0.75f);
+                if (heroEnFume[i].estVivant)
+                {
+                    spriteBatch.Draw(heroEnFume[i].sprite, heroEnFume[i].position, null, Color.White,
+                    hero.angleRotation, hero.origine, 0.4f, SpriteEffects.None, 1.0f);
+                }
             }
+            
             //Les 16 ennemis
             for (int i = 0; i < 16; i++)
             {
@@ -446,7 +672,7 @@ namespace Projet_02
             }
             
             // Écrire un texte
-            spriteBatch.DrawString(font, "Hello Mono!", new Vector2(100, 100), Color.Black);
+            spriteBatch.DrawString(font, message, new Vector2(100, 100), Color.Black);
             spriteBatch.End();
 
             base.Draw(gameTime);
